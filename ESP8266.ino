f84ed99919006f884e56762bb759412046448d76
djoele <dennisjoele@hotmail.com>
2019-06-15 22:16:40 +0200

    Update ESP8266.ino

--- ESP8266.ino
+++ ESP8266.ino
@@ -1,3 +1,5 @@
+#include <Ethernet.h>
+
 #define DEBUG
 #include <EEPROM.h>
 #include <FS.h>
@@ -21,66 +23,68 @@
 #include <Wire.h>
 
 void setup() {
-  #ifdef DEBUG
-    Serial.begin(115200);
-    Serial.setDebugOutput(true);
-  #endif
-  
+#ifdef DEBUG
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+#endif
+
   EEPROM.begin(2000);
   SPIFFS.begin();
 
   version = readFile("/md5.txt");
   strcpy(md5value, version.c_str());
-  
+  Serial.println((String("[MD5] md5: ") + md5value));
+
   sha = readFile("/sha.txt");
   fingerprint = sha.c_str();
   strcpy(shavalue, sha.c_str());
-  
-  memset(unameenc,0,sizeof(unameenc));
+  Serial.println((String("[SHA] sha: ") + shavalue));
+
+  memset(unameenc, 0, sizeof(unameenc));
   base64_encode(unameenc, uname, strlen(uname));
 
-  connectWifi();     
   determineStartValues();
-  
+
+  connectWifi();
+
   pinMode(pinWater, INPUT_PULLUP);
   waarde = digitalRead(pinWater);
   waardenu = waarde;
 
-  Alarm.timerRepeat(300, uploadWater);
+  Alarm.timerRepeat(60, uploadWater);
   Alarm.timerRepeat(120, saveValues);
-  Alarm.timerRepeat(60, reconnectWifi);
-  
-  #ifdef DEBUG
-    telnetServer.begin();
-    telnetServer.setNoDelay(true);
-  #endif
+  Alarm.timerRepeat(20, reconnectWifi);
 
-  server.on("/update_esp8266", [](){
-    if(!server.authenticate(www_username, www_password))
+#ifdef DEBUG
+  telnetServer.begin();
+  telnetServer.setNoDelay(true);
+#endif
+
+  server.on("/update_esp8266", []() {
+    if (!server.authenticate(www_username, www_password))
       return server.requestAuthentication();
     server.send(200, "text/plain", "Login Succes, updating start..");
     doUpdate();
   });
-  server.on("/reset", [](){
-    if(!server.authenticate(www_username, www_password))
+  server.on("/reset", []() {
+    if (!server.authenticate(www_username, www_password))
       return server.requestAuthentication();
     server.send(200, "text/plain", "ESP8266 gaat resetten..");
     ESP.reset();
   });
-  server.on("/resetvalues", [](){
-    if(!server.authenticate(www_username, www_password))
+  server.on("/resetvalues", []() {
+    if (!server.authenticate(www_username, www_password))
       return server.requestAuthentication();
     int reset = atoi(server.arg("reset").c_str());
-    server.send(200, "text/plain", "ESP8266 gaat value resetten naar " + reset);
     resetStartValues(reset);
   });
-  server.on("/stack", [](){
-    if(!server.authenticate(www_username, www_password))
+  server.on("/stack", []() {
+    if (!server.authenticate(www_username, www_password))
       return server.requestAuthentication();
-      server.send(200, "text/plain", loadStack());
+    server.send(200, "text/plain", loadStack());
   });
-  server.on("/resetinfo", [](){
-    if(!server.authenticate(www_username, www_password))
+  server.on("/resetinfo", []() {
+    if (!server.authenticate(www_username, www_password))
       return server.requestAuthentication();
     server.send(200, "text/plain", ESP.getResetInfo());
     //Nu hebben we de stack trace ook echt verstuurd
@@ -88,13 +92,9 @@ void setup() {
     eeprom_erase_all();
     EEPROM.commit();
   });
-  server.on("/update_sha", HTTP_POST, [](){
-    if(!server.authenticate(www_username, www_password))
+  server.on("/update_sha", HTTP_POST, []() {
+    if (!server.authenticate(www_username, www_password))
       return server.requestAuthentication();
-    server.send(200, "text/plain", "Login Succes, updating sha..");    
-    #ifdef DEBUG
-      serverClient.println(F("[SHA] Login Succes, updating sha.."));
-    #endif 
     saveSHA(server.arg("sha"));
     sha = readFile("/sha.txt");
     strcpy(shavalue, sha.c_str());
@@ -105,28 +105,28 @@ void setup() {
   });
   server.begin();
 
-  triggerStack();
+  //triggerStack();
 }
 
 
-void loop() { 
+void loop() {
   trigger_nu = now();
   tijdsduur = trigger_nu - trigger_begin;
   //#ifdef DEBUG
-      //serverClient.println(String("[DUUR] Tijdsduur geen puls gezien: ") + tijdsduur);
-    //#endif
-  
-  #ifdef DEBUG
-    server.handleClient();
-    handleTelnet();
-  #endif
-  
+  //serverClient.println(String("[DUUR] Tijdsduur geen puls gezien: ") + tijdsduur);
+  //#endif
+
+#ifdef DEBUG
+  server.handleClient();
+  handleTelnet();
+#endif
+
   pulsetaskwater();
-  if (waterpuls == 1){
+  if (waterpuls == 1) {
     counter++;
-    #ifdef DEBUG
-      serverClient.println(String("[PULS] Waterpuls gedetecteerd: ") + counter);
-    #endif
+#ifdef DEBUG
+    serverClient.println(String("[PULS] Waterpuls gedetecteerd: ") + counter);
+#endif
     waterpuls = 0;
     trigger_begin = now();
   }
